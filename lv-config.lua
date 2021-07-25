0d386dcad56128c335e4eec720b6c8bbae636d03
William Goulois <william.goulois@fabernovel.com>
2021-07-25 15:09:12 +0200

    update to new_contract

--- lv-config.lua
+++ lv-config.lua
@@ -9,71 +9,81 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-
-O.format_on_save = true
-O.lint_on_save = true
-O.completion.autocomplete = true
-O.colorscheme = "spacegray"
-O.default_options.wrap = true
-O.default_options.timeoutlen = 100
-O.leader_key = " "
+lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "spacegray"
+lvim.leader = "space"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-O.plugin.dashboard.active = true
-O.plugin.terminal.active = true
-O.plugin.zen.active = false
-O.plugin.zen.window.height = 0.90
-O.plugin.nvimtree.side = "left"
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- dashboard
-O.plugin.dashboard.custom_header = {
-  "            ██                                                                                           ",
-  "           ███                                                                                           ",
-  "          ▓███                                                                                           ",
-  "▓████████▌                                                                                               ",
-  "█████████                                                                                                ",
-  "████████                                                                                                 ",
-  "╙╙╙╙╙╙╙█▓▓b         @▓▓▓▓    ▄▓µ     ▓▓▓▓▌    ▓▓▓▓▌   ▓▓▓▓▄   ▐▓µ  ▓⌐   ,▄▓▓▓▄   %▓   ▄▓   @▓▓▓▓   ▓▌    ",
-  "      ▐███▌         ██▄▄    ▐█╙█     ██▄▄█▀   █▌▄▄   j█▄ ▄█⌐  ╫██▌ █µ  ▐█╨   ██   ██ ▐█─   ╫█▄▄    ██    ",
-  "     .████▌         ██└└   ╓██▓██    █▌ └██   █▌└└   j█▀▀█▄   ╫█ ▀██µ  ╙█▄  ,██    ███▀    ╫█└└    ██    ",
-  "▄▄▄▄▄╙▀▀▀▀¬         ╙▀     ▀╙   ▀▀   ▀▀▀▀▀    ▀▀▀▀▀   ▀  ╙▀   ╙▀  ╙▀     ▀▀▀▀└      ▀▀     ╙▀▀▀▀   ▀▀▀▀▀ ",
-  "████▀                                                                                                    ",
-  "███▌                                                                                                     ",
-  "███                                                                                                      ",
-  "██                                                                                                       ",
-  "█╜                                                                                                       ",
+lvim.builtin.dashboard.custom_header = {
+	"            ██                                                                                           ",
+	"           ███                                                                                           ",
+	"          ▓███                                                                                           ",
+	"▓████████▌                                                                                               ",
+	"█████████                                                                                                ",
+	"████████                                                                                                 ",
+	"╙╙╙╙╙╙╙█▓▓b         @▓▓▓▓    ▄▓µ     ▓▓▓▓▌    ▓▓▓▓▌   ▓▓▓▓▄   ▐▓µ  ▓⌐   ,▄▓▓▓▄   %▓   ▄▓   @▓▓▓▓   ▓▌    ",
+	"      ▐███▌         ██▄▄    ▐█╙█     ██▄▄█▀   █▌▄▄   j█▄ ▄█⌐  ╫██▌ █µ  ▐█╨   ██   ██ ▐█─   ╫█▄▄    ██    ",
+	"     .████▌         ██└└   ╓██▓██    █▌ └██   █▌└└   j█▀▀█▄   ╫█ ▀██µ  ╙█▄  ,██    ███▀    ╫█└└    ██    ",
+	"▄▄▄▄▄╙▀▀▀▀¬         ╙▀     ▀╙   ▀▀   ▀▀▀▀▀    ▀▀▀▀▀   ▀  ╙▀   ╙▀  ╙▀     ▀▀▀▀└      ▀▀     ╙▀▀▀▀   ▀▀▀▀▀ ",
+	"████▀                                                                                                    ",
+	"███▌                                                                                                     ",
+	"███                                                                                                      ",
+	"██                                                                                                       ",
+	"█╜                                                                                                       ",
 }
-O.plugin.dashboard.footer = { "Welcome to LunarVim" }
+lvim.builtin.dashboard.footer = { "Welcome to LunarVim" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
-O.treesitter.ensure_installed = "maintained"
-O.treesitter.ignore_install = { "haskell" }
-O.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+lvim.lsp.diagnostics.virtual_text = false
 
 -- javascript
-O.lang.tsserver.linter = "eslint_d"
-O.lang.tsserver.formatter.exe = "prettier"
+lvim.lang.javascript.linters = { "eslint_d" }
+lvim.lang.javascriptreact.linters = { "eslint_d" }
+lvim.lang.typescript.linters = { "eslint_d" }
+lvim.lang.typescriptreact.linters = { "eslint_d" }
+
+require("lv-utils").add_keymap_insert_mode({ silent = true, expr = true }, {
+	{ "<right>", "compe#confirm('<CR>')" },
+})
 
 -- Additional Plugins
-O.user_plugins = {
-  {
-    "mg979/vim-visual-multi",
-    branch = "master",
-  },
-  {
-    "p00f/nvim-ts-rainbow",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        rainbow = {
-          enable = true,
-          extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-          max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-        },
-      }
-    end,
-  },
+lvim.plugins = {
+	{
+		"mg979/vim-visual-multi",
+		branch = "master",
+	},
+	{
+		"p00f/nvim-ts-rainbow",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				rainbow = {
+					enable = true,
+					extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+					max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+				},
+			})
+		end,
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		config = function()
+			require("lsp_signature").on_attach()
+		end,
+		event = "InsertEnter",
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
+-- O.user_autocommands = {{ "CursorMoved", "*.tsx", "lua echo_diagnostic()"}}
